0ed4eab41f6c85041787db1bd57c5d03c4dd6fcd
miyagi-0817 <82007331+miyagi-0817@users.noreply.github.com>
2021-08-05 21:24:23 +0900

    Update Room.js

--- pages/Room.js
+++ pages/Room.js
@@ -1,30 +1,6 @@
-// import { useForm, Controller } from "react-hook-form";
+import React from "react";
 
-// import Container from "@material-ui/core/Container";
-// import { TextField } from "@material-ui/core";
-// import Input from "@material-ui/core/Input";
-// import { render } from "react-dom";
-// import firebase from "../config/firebase";
-// import React, { useEffect } from 'react'
-
-// const Room = () => {
-//     const [questioneares, setQuestioneares] = useState(null)
-//     const [value, setValue] = useState('')
-
-// useEffect(() => {
-//   firebase
-//     .firestore()
-//     .collection("questionnaires")
-//     .onSnapshot(() => {
-//       const questionnaires = snapshot.docs.map((doc) => {
-//         return doc.data();
-//       });
-//  setQuestioneares(questioneares)
-
-//     });
-// }, []);
-
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import firebase from "../config/firebase";
 
 const Room = () => {
